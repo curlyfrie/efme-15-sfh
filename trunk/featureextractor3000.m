a8d71e908bda0374c1ef4afa8c832ee6db5998e7
hiller.elias <hiller.elias@b178910a-905a-21d0-cc70-2735015b2e23>
2010-03-23 15:13:13 +0000

    new genius version

--- trunk/featureextractor3000.m
+++ trunk/featureextractor3000.m
@@ -9,10 +9,13 @@ P = cell(100,1);
 
 j=1; 
 
+       S = zeros(20,4);
+o = 0;
 for i = 1 : size( D, 1 )
    
       
-    if D( i ).isdir == 0 && (strncmp( D( i ).name( 1 : 3 ) , 'apple' , 3) || strncmp( D( i ).name( 1 : 3 ) , 'bat' , 3) || strncmp( D( i ).name( 1 : 3 ) , 'bird' , 3) || strncmp( D( i ).name( 1 : 3 ) , 'Bone' , 3) || strncmp( D( i ).name( 1 : 3 ) , 'beetle' , 3))
+    if D( i ).isdir == 0 && (strncmp( D( i ).name( 1 : 3 ) , 'beetle' , 3) )
+        %|| strncmp( D( i ).name( 1 : 3 ) , 'bat' , 3) || strncmp( D( i ).name( 1 : 3 ) , 'bird' , 3) || strncmp( D( i ).name( 1 : 3 ) , 'Bone' , 3) || strncmp( D( i ).name( 1 : 3 ) , 'beetle' , 3))
      
        I_temp = imread (fullfile(directory,D(i).name))  ;
        I{j} = im2bw( I_temp , graythresh ( I_temp ) ) ;
@@ -20,10 +23,81 @@ for i = 1 : size( D, 1 )
        
        
        STATS = regionprops(I{j},'all');
-       P{j} = STATS
+      % P{j} = STATS;
+       
+       
+      % only proceed with the max area
+       [area,ind] = max([STATS.Area]);
+       perimeter = (STATS(ind).Perimeter);
+       majoraxis =  (STATS(ind).MajorAxisLength);
+       minoraxis = (STATS(ind).MinorAxisLength);
+       
+       
+       % ignore blobs < 30
+       %STATS = STATS([STATS.Area] > 30 );
+       % STATS = sort([STATS.Area],1)
+            
+       
+       
+
+       
+       %formfactor:
+       formfactor = (4*pi*area) ./ perimeter.^2;
+       S(j,1) = formfactor;
+     
+       %roundness:
+       roundness = (4*area) / (pi*majoraxis^2);
+       S(j,2) = roundness;
+       
+       
+       %compactness:
+       compactness = sqrt(roundness);
+       S(j,3) = compactness;
+       
+       %aspect ratio:
+       aspectratio =  majoraxis / minoraxis;
+       S(j,4) = aspectratio;
+       
+       
+       %orientation solidity
+       %convexity = STATS.Perime
+       
+       %position, orientation and scale invariant?
+       
+       
+       % apple
+       if STATS(ind).Solidity >= 0.8846 && STATS(ind).Solidity <= 0.9702
+           disp(strcat('Ich bin ein Apfel -> ',D(i).name))
+           %o=o+1
+       % bone
+       elseif roundness >= 0.0775 && roundness <= 0.1547
+            disp(strcat('Ich bin eine Bohne, joke, ich bin ein Knochen -> ',D(i).name))
+       % beetle
+       elseif formfactor >= 0.0338 && formfactor <= 0.1083 
+            disp(strcat('Ich bin ein Käfer -> ',D(i).name))
+       end
+        
+       
+       
+       
+       
+       
+       
+       
        j = j +1;
     end
 end
+
+
+
+%min (S(:,1))
+%max (S(:,1))
+%min (S(:,2))
+%max (S(:,2))
+%min (S(:,3))
+%max (S(:,3))
+%min (S(:,4))
+%max (S(:,4))
 
 
 
