6c66caeccb207ca2087968ef7cdce8eeb1098400
hiller.elias <hiller.elias@b178910a-905a-21d0-cc70-2735015b2e23>
2010-03-28 19:37:47 +0000

    neues file.

---
+++ trunk/histerie.m
@@ -0,0 +1,26 @@
+function [] = histerie ()
+
+
+img = imread('walter.png');
+
+ind = 1;
+blocks = cell(256,1);
+
+%block groesse berechnen
+bsize_col = floor(size(img,2)/16);
+bsize_row = floor(size(img,1)/16);
+for i=1:16
+    for j=1:16
+        %matrix des blocks auslesen
+        blocks{ind} = img(((i-1)*bsize_row)+1:i*bsize_row, ((j-1)*bsize_col)+1:j*bsize_col);
+        ind=ind+1;
+    end
+end
+
+for i=1:6
+    subplot(6,1,i);
+    imhist(blocks{i});
+end
+
+    
+ 
