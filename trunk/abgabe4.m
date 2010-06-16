f15daa526f681a246afb39e52531f3f953f26376
danielfritz1 <danielfritz1@b178910a-905a-21d0-cc70-2735015b2e23>
2010-06-16 11:17:42 +0000

    included kNN, fin.

--- trunk/abgabe4.m
+++ trunk/abgabe4.m
@@ -2,6 +2,9 @@
 % 
 function abgabe4
 
+close all;
+clear;
+
 file = 'data/pima-indians-diabetes.data';
 data = load(file);
 
@@ -9,6 +12,10 @@ training = data(1:500, :);
 testing = data(501:768, :);
 PICS = length(testing);
 
+%MAHALANOBIS
+%%%%%%%%%%%%%
+
+%with all features
 disp(['Mahalnobis with all features (training 1-500, test 501-768)',10]);
 mahala (training, testing, PICS)
 
@@ -16,6 +23,17 @@ mahala (training, testing, PICS)
 disp([10,10,'Mahalnobis without feature 4 and 5 (training 1-500, test 501-768)',10]);
 mahala (training(:,[1:3 6:9]), testing(:,[1:3 6:9]), PICS)
 
+%KNN
+%%%%%%%%%%%%%
+
+kNN = 20;
+%with all features
+disp([10,10,'kNN with all features (training 1-500, test 501-768)',10]);
+knn(training, testing, PICS,kNN,'kNN with all features')
+
+%without features 4 and 5, because most of them are 0 (mode(data(:,4)) = 0)
+disp([10,10,'kNN without feature 4 and 5 (training 1-500, test 501-768)',10]);
+knn (training(:,[1:3 6:9]), testing(:,[1:3 6:9]), PICS,kNN, 'kNN without feature 4 and 5')
 
 function mahala (training, testing, PICS)
 
@@ -111,15 +129,75 @@ for CONTROL = 1:4
     end
 
     sumerrors = sum(errors);
-    prozent = (sumerrors*100)/PICS;
+    percent = (sumerrors*100)/PICS;
  
     if CONTROL == 1
-        disp(['full covarianz for a class: sumerrors = ', num2str(sumerrors),' prozent = ', num2str(prozent)]);
+        disp(['full covarianz for a class: sumerrors = ', num2str(sumerrors),' percent = ', num2str(percent)]);
     elseif CONTROL == 2
-        disp(['covarianz diagonal: sumerrors = ', num2str(sumerrors),' prozent = ', num2str(prozent)]);
+        disp(['covarianz diagonal: sumerrors = ', num2str(sumerrors),' percent = ', num2str(percent)]);
     elseif CONTROL == 3
-        disp(['covarianz overall: sumerrors = ', num2str(sumerrors),' prozent = ', num2str(prozent)]);
+        disp(['covarianz overall: sumerrors = ', num2str(sumerrors),' percent = ', num2str(percent)]);
     elseif CONTROL == 4 
-        disp(['covarianz overall mean: sumerrors = ', num2str(sumerrors),' prozent = ', num2str(prozent)]);
+        disp(['covarianz overall mean: sumerrors = ', num2str(sumerrors),' percent = ', num2str(percent)]);
     end
 end
+
+function knn(training, testing, PICS, kNN, name)
+
+
+%declaration of data types
+eucl = cell(PICS,2);
+errors = zeros(PICS,kNN);
+
+class = size(training,2);
+
+%iterate through test data
+for k=1:PICS
+    %iterate through trainigsdata and calculate euclid distance
+    for l=1:length(training)
+
+        eucl{l,1} = sqrt(sum((training(l,1:class-1)-testing(k,1:class-1)).^2));
+        eucl{l,2} = training(l,class);
+    end
+
+    % get class of current picture
+    curclass = testing(k,class);
+
+    % now sort it
+    [distances] = sortrows(eucl);
+    
+    %error test every picture if wrong or right
+    %needed for the graph 
+    for v=1:kNN
+        if mode([distances{1:v,2}])~=curclass 
+            errors(k,v)=1;
+        end
+    end
+    
+end
+
+
+%     sumerrors = sum(errors)
+%     percent = (sumerrors*100)/PICS
+
+
+% calculate graphs and make a wonderful looking plot
+% sumerrors = zeros(1,kNN);
+%  for p=1:kNN 
+%          sumerrors(p) = sum(errors(:,p));
+%  end
+
+sumerrors = sum(errors);
+percent = (sumerrors*100)/PICS;
+
+%display best kNN value and the corresponding values
+[theerror, bestknn]=min(sumerrors)
+thepercent = min(percent)
+
+figure('Name',['Errorrate ', name],'NumberTitle','off')
+plot(1:kNN,percent);
+title(name);
+xlabel('kNN')
+ylabel('Errorrate %')
+axis([0 kNN  15 40]);
+
